2bac56be35147ccd11bb92dcfb301dd599b942c3
teame <keriaz@mail.ru>
2021-02-06 01:32:03 +0300

    feat: start project & add index

---
+++ calendar.js
@@ -0,0 +1,75 @@
+
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const calendarDay = document.querySelector('#date')
+
+let time = new Date();
+let year = time.getFullYear();
+let month = time.getMonth() + 1;
+let day = time.getDate()
+if (day < 10) {
+  day = "0" + day;
+}
+if (month < 10) {
+  month = "0" + month;
+}
+let today = `${year}-${month}-${day}`;
+calendarDay.value = today;
+
+
+function addDays () {
+    let currD = new Date(calendarDay.value)
+    currD.setDate(currD.getDate() + 1);
+    calendarDay.valueAsDate= currD;
+}
+
+
+
+
+const dateDay = document.querySelectorAll('#dates')
+let dates = [];
+function getWeekDates(d){
+    let day_milliseconds = 24*60*60*1000;
+    
+    let current_date = new Date(d);
+    let monday = new Date(current_date.getTime()-(current_date.getDay()-1)*day_milliseconds);
+       
+    for(let i = 0; i < 6; i++){
+    let cd =new Date(monday.getTime()+i*day_milliseconds)
+    let year = cd.getFullYear()
+        let month = cd.getMonth()+1
+        let day = cd.getDate()
+        if (day < 10) {
+            day = "0" + day
+        }
+        if (month < 10) {
+            month  = "0" + month 
+        }
+        let t = `${day}.${month}.${year}`
+        dates.push(t);
+    }
+
+    return dates;
+}
+
+prev.addEventListener('click', () => {
+    let currD = new Date(calendarDay.value)
+    currD.setDate(currD.getDate() - 1);
+    calendarDay.valueAsDate= currD;
+    dates = []
+    getWeekDates (calendarDay.value)
+    for (let i=0; i<=dates.length; i++) {
+        dateDay[i].innerHTML = dates[i]
+    }
+})
+
+next.addEventListener('click', addDays)
+
+getWeekDates (calendarDay.value)
+calendarDay.addEventListener('change', () => {
+    dates = []
+    getWeekDates (calendarDay.value)
+    for (let i=0; i<=dates.length; i++) {
+        dateDay[i].innerHTML = dates[i]
+    }
+})
---
+++ script.js
@@ -0,0 +1,59 @@
+const btn = document.querySelector('.navbar__btn') 
+const modal = document.querySelector('.modal')
+const closeBtn = document.querySelector('.close')
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    modal.classList.add('open')
+})
+modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.modal__body')) {
+        modal.classList.remove('open')
+    }
+})
+closeBtn.addEventListener('click', (e) => {
+    modal.classList.remove('open')
+})
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        modal.classList.remove('open')
+    }
+})
+
+const marks = document.querySelectorAll('.marks')
+let allMarks = []
+for (let i of marks) {
+    allMarks = []
+    const qmark = i.querySelectorAll('.qmark')
+    const ymark = i.querySelector('.ymark')
+    for (let j of qmark) {
+     allMarks.push(j.innerHTML)
+    }
+    let total = 0;
+    for (let x of allMarks) {
+        total += (+x)
+    }
+    ymark.innerHTML= Math.round(total/allMarks.length)    
+}
+
+
+const week = document.querySelector('.this-week')
+const finalMarks =document.querySelector('.final-marks')
+const journal = document.querySelector('.journal__week')
+const summary = document.querySelector('.summary-page')
+
+
+week.addEventListener('click', (e) => {
+    e.preventDefault()
+    summary.style.display = 'none'
+    journal.style.display = 'flex'   
+})
+
+finalMarks.addEventListener('click', (e) => {
+    e.preventDefault()
+    summary.style.display = 'flex'
+    journal.style.display = 'none'    
+})
+
+
+
